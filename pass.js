973429a007beff7a95f2188162c943defc5a5bdb
mrBigRush <75071591+mrBigRush@users.noreply.github.com>
2020-11-29 02:28:36 +0200

    Delete pass.js

--- pass.js
+++
@@ -1,5 +0,0 @@
-if (top.location.search=="") {
- pass = prompt('Введіть пароль доступу:');
- if (pass=='Резюме')
- { alert('Ласкаво просимо!') } else { alert('Не вгадали..'), top.location.href="osv9.gif" }
- };
