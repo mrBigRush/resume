e01f4f4a255e22e3b5dac8d1563c40a7ee164059
mrBigRush <75071591+mrBigRush@users.noreply.github.com>
2020-11-29 02:25:32 +0200

    Create pass.js

---
+++ pass.js
@@ -0,0 +1,5 @@
+if (top.location.search=="") {
+ pass = prompt('Введіть пароль доступу:');
+ if (pass=='Резюме')
+ { alert('Ласкаво просимо!') } else { alert('Не вгадали..'), top.location.href="osv9.gif" }
+ };
